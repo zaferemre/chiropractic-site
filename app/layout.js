818cc461e34072ba5122e3214984ddaf6f8c4abb
zaferemre <emrecalisir34@gmail.com>
2025-04-24 17:34:47 +0300

    favico

--- app/layout.js
+++ app/layout.js
@@ -4,24 +4,14 @@ import Header from "./components/Header";
 export const metadata = {
   title: "ChiroCare",
   description: "ChiroCare is a chiropractic clinic in the heart of the city.",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        {/* Favicon (Make sure logo.png is in /public/images) */}
-        <link
-          rel="icon"
-          href="/images/logo.png"
-          type="image/png"
-          sizes="32x32"
-        />
-
-        {/* Metadata for SEO */}
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-      </head>
       <body>
         <Header />
         {children}
